dfb41465b681ff92b44299c7b46b8b375877aacc
KleberPF <43550602+KleberPF@users.noreply.github.com>
2021-07-12 21:53:28 -0300

    fixed memory leaks and improved code

--- main.c
+++ main.c
@@ -2,10 +2,24 @@
 #include <windows.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
 
+// screen dimensions
 #define SCREEN_WIDTH 1600
 #define SCREEN_HEIGHT 900
+
+// coordinates of the top left square
+#define TOP_LEFT_X 612
+#define TOP_LEFT_Y 270
+
+// offset between two squares
+#define SQUARE_OFFSET 150
+
+// delay between clicks (in ms)
+#define CLICK_DELAY 1
+
+// delay between screenshots
+#define SCRSHOT_DELAY 1
 
 void mouseSetup(INPUT* input)
 {
@@ -38,14 +52,12 @@ void mouseClick(INPUT* input)
 PBITMAPINFO createBitmapInfoStruct(HBITMAP hBmp)
 {
     BITMAP bmp = { 0 };
-    PBITMAPINFO bmpInfo;
-    WORD clrBitsCount;
 
     // retrieve the bitmap color format, width and height
-    assert(GetObject(hBmp, sizeof(BITMAP), (LPSTR)&bmp));
+    GetObject(hBmp, sizeof(BITMAP), (LPSTR)&bmp);
 
     // convert the color format to a count of bits
-    clrBitsCount = (WORD)(bmp.bmPlanes * bmp.bmBitsPixel);
+    WORD clrBitsCount = (WORD)(bmp.bmPlanes * bmp.bmBitsPixel);
 
     if (clrBitsCount == 1) { clrBitsCount = 1; }
     else if (clrBitsCount <= 4) { clrBitsCount = 4; }
@@ -55,49 +67,47 @@ PBITMAPINFO createBitmapInfoStruct(HBITMAP hBmp)
     else { clrBitsCount = 32; }
 
     // allocate memory for the BITMAPINFO struct, which contains a BITMAPINFOHEADER and an array of RGBQUAD
+    PBITMAPINFO bmpInfo;
     if (clrBitsCount < 24)
     {
-        bmpInfo = (PBITMAPINFO)LocalAlloc(LPTR, sizeof(BITMAPINFOHEADER) + sizeof(RGBQUAD) * (1 << clrBitsCount));
+        bmpInfo = malloc(sizeof(BITMAPINFOHEADER) + sizeof(RGBQUAD) * ((SIZE_T)1 << clrBitsCount));
     }
     else // there is no RGBQUAD array for 24-bit and 32-bit
     {
-        bmpInfo = (PBITMAPINFO)LocalAlloc(LPTR, sizeof(BITMAPINFOHEADER));
+        bmpInfo = malloc(sizeof(BITMAPINFOHEADER));
     }
-    assert(bmpInfo);
 
     // initialize the fields of the BITMAPINFO struct
-    bmpInfo->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    bmpInfo->bmiHeader.biWidth = bmp.bmWidth;
-    bmpInfo->bmiHeader.biHeight = bmp.bmHeight;
-    bmpInfo->bmiHeader.biPlanes = bmp.bmPlanes;
-    bmpInfo->bmiHeader.biBitCount = bmp.bmBitsPixel;
-    if (clrBitsCount < 24) { bmpInfo->bmiHeader.biClrUsed = (1 << clrBitsCount); }
+    if (bmpInfo != NULL)
+    {
+        bmpInfo->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+        bmpInfo->bmiHeader.biWidth = bmp.bmWidth;
+        bmpInfo->bmiHeader.biHeight = bmp.bmHeight;
+        bmpInfo->bmiHeader.biPlanes = bmp.bmPlanes;
+        bmpInfo->bmiHeader.biBitCount = bmp.bmBitsPixel;
+        if (clrBitsCount < 24) { bmpInfo->bmiHeader.biClrUsed = (1 << clrBitsCount); }
 
-    // if the bitmap is not compressed, set the BI_RGB flag
-    bmpInfo->bmiHeader.biCompression = BI_RGB;
-    bmpInfo->bmiHeader.biSizeImage = ((bmpInfo->bmiHeader.biWidth * clrBitsCount + 31) & ~31) / 8 * bmpInfo->bmiHeader.biHeight;
-    bmpInfo->bmiHeader.biClrImportant = 0;
+        // if the bitmap is not compressed, set the BI_RGB flag
+        bmpInfo->bmiHeader.biCompression = BI_RGB;
+        bmpInfo->bmiHeader.biSizeImage = ((bmpInfo->bmiHeader.biWidth * clrBitsCount + 31) & ~31) / 8 * bmpInfo->bmiHeader.biHeight;
+        bmpInfo->bmiHeader.biClrImportant = 0;
+    }
 
     return bmpInfo;
 }
 
 DWORD* getBitmapPixelArray(HBITMAP hBmp)
 {
-    PBITMAPINFOHEADER bmpInfoHeader;
-    LPBYTE bmpBits;
-    PBITMAPINFO bmpInfo;
-    HDC hdc;
-
-    hdc = CreateCompatibleDC(GetWindowDC(GetDesktopWindow()));
+    HDC hdc = CreateCompatibleDC(GetWindowDC(GetDesktopWindow()));
     SelectObject(hdc, hBmp);
-    bmpInfo = createBitmapInfoStruct(hBmp);
+    PBITMAPINFO bmpInfo = createBitmapInfoStruct(hBmp);
 
-    bmpInfoHeader = (PBITMAPINFOHEADER)bmpInfo;
-    bmpBits = (LPBYTE)GlobalAlloc(GMEM_FIXED, bmpInfoHeader->biSizeImage);
-    assert(bmpBits);
+    PBITMAPINFOHEADER bmpInfoHeader = (PBITMAPINFOHEADER)bmpInfo;
+    LPBYTE bmpBits = malloc(SCREEN_HEIGHT * SCREEN_WIDTH * 4);
 
     // retrieve the color table (RGBQUAD array) and the bits (array of pallete indices) from the DIB
-    assert(GetDIBits(hdc, hBmp, 0, (WORD)bmpInfoHeader->biHeight, bmpBits, bmpInfo, DIB_RGB_COLORS));
+    GetDIBits(hdc, hBmp, 0, (WORD)bmpInfoHeader->biHeight, bmpBits, bmpInfo, DIB_RGB_COLORS);
+    free(bmpInfo);
 
     return (DWORD*)bmpBits;
 }
@@ -115,15 +125,12 @@ HBITMAP getScreenBitmap()
     // and a device context to put it in
     HDC hMemoryDC = CreateCompatibleDC(hScreenDC);
 
-    int width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
-    int height = GetSystemMetrics(SM_CYVIRTUALSCREEN);
-
-    HBITMAP hBitmap = CreateCompatibleBitmap(hScreenDC, width, height);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hScreenDC, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     // get a new bitmap
     HBITMAP hOldBitmap = (HBITMAP)SelectObject(hMemoryDC, hBitmap);
 
-    BitBlt(hMemoryDC, 0, 0, width, height, hScreenDC, 0, 0, SRCCOPY);
+    BitBlt(hMemoryDC, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, hScreenDC, 0, 0, SRCCOPY);
     hBitmap = (HBITMAP)SelectObject(hMemoryDC, hOldBitmap);
 
     // clean up
@@ -135,37 +142,28 @@ HBITMAP getScreenBitmap()
 
 void checkBlackSquares(DWORD* pixelArr, int playingField[4][4])
 {
-    // top left square coordinate
-    int topLeftX = 612;
-    int topLeftY = 270;
-    int offset = 150;
-
     for (int i = 0; i < 4; ++i)
     {
         for (int j = 0; j < 4; ++j)
         {
             // 1 if pixel is black, 0 otherwise
-            playingField[i][j] = getPixelColorAtCoord(pixelArr, topLeftX + j * offset, topLeftY + i * offset) < 0xff000011;
+            playingField[i][j] = getPixelColorAtCoord(pixelArr, TOP_LEFT_X + j * SQUARE_OFFSET, 
+                TOP_LEFT_Y + i * SQUARE_OFFSET) < 0xff000011;
         }
     }
 }
 
 void clickSquares(INPUT input, int playingField[4][4])
 {
-    // top left square coordinate
-    int topLeftX = 612;
-    int topLeftY = 270;
-    int offset = 150;
-
     for (int i = 0; i < 4; ++i)
     {
         for (int j = 0; j < 4; ++j)
         {
             if (playingField[i][j])
             {
-                mouseMoveAbsolute(&input, topLeftX + j * offset, topLeftY + i * offset);
+                mouseMoveAbsolute(&input, TOP_LEFT_X + j * SQUARE_OFFSET, TOP_LEFT_Y + i * SQUARE_OFFSET);
                 mouseClick(&input);
-                Sleep(20);
+                Sleep(CLICK_DELAY);
             }
         }
     }
@@ -187,9 +185,9 @@ int main()
             int playingField[4][4];
             checkBlackSquares(pixelArr, playingField);
             clickSquares(input, playingField);
-            Sleep(40);
+            Sleep(SCRSHOT_DELAY);
 
-            GlobalFree((HGLOBAL)pixelArr);
+            free(pixelArr);
         }
         if (GetAsyncKeyState(VK_ESCAPE))
         {
